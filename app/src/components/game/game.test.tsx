10c210f393cea776d574e27987b17063d3c7184c
Dillon Drenzek <dillon@drenzek.com>
2020-06-29 22:30:21 -0700

    init game component

---
+++ app/src/components/game/game.test.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+describe('Game', () => {
+  xit('is true', () => { expect(true).toBe(false); })
+});
---
+++ app/src/components/game/game.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Cell, ICell, CellState } from '../cell/cell';
+import { GridHelper } from '../../lib/grid-helper';
+
+import './grid.scss';
+
+export interface GameProps {
+  width: number;
+  height: number;
+}
+
+export const Game: React.FunctionComponent<GameProps> = (props) => {
+  return null;
+}
