561ef6d81d8ab1c53a89fa1362c43a66459cc172
Dillon Drenzek <dillon@drenzek.com>
2021-04-24 17:08:31 -0700

    code cleanup

--- spec/helpers/Grid-helpers.js
+++
@@ -1,9 +0,0 @@
-const expectGridsEqual = function (a, b) {
-  for(let y = 0; y < a.height; y++) {
-    for(let x = 0; x < a.width; x++) {
-      expect(a.get(x,y)).toEqual(b.get(x,y));
-    }
-  }
-}
-
-module.exports = { expectGridsEqual };
