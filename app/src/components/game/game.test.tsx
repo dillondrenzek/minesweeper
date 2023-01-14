d802127e282927d00a39e5708be0116d791403d8
Dillon Drenzek <dillon@drenzek.com>
2023-01-14 10:28:03 -0800

    fix test (#16)

--- app/src/components/game/game.test.tsx
+++ app/src/components/game/game.test.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { Game } from './game';
+import React from "react";
+import { Game } from "./game";
 
-describe('Game', () => {
-  it('does not throw', () => {
-    expect(() => <Game />).not.toThrow();
+describe("Game", () => {
+  it("does not throw", () => {
+    expect(() => (
+      <Game config={{ width: 10, height: 10, numMines: 10 }} />
+    )).not.toThrow();
   });
 });
